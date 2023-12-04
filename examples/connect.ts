358dc8dc520971a3ee9a2bc238d74070a5de9d21
meisZWFLZ <atcurtis08@gmail.com>
2023-12-03 20:14:26 -0700

    style: :art: format code

--- examples/connect.ts
+++ examples/connect.ts
@@ -1,10 +1,10 @@
 import { serial } from "node-web-serial-ponyfill";
 import { V5SerialDevice } from "../src";
 
-(async function() {
-    let device = new V5SerialDevice(serial);
+(async function () {
+  let device = new V5SerialDevice(serial);
 
-    await device.connect();
+  await device.connect();
 
-    console.log(device.brain.systemVersion);
+  console.log(device.brain.systemVersion);
 })();
--- test/VexEvent.spec.ts
+++ test/VexEvent.spec.ts
@@ -1,44 +1,43 @@
 import { VexEventTarget } from "../src";
 
 function getRandomInt(max: number) {
-    return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max);
 }
 
 test("VexEventEmitter and VexEventTarget", async () => {
-    let target = new VexEventTarget();
+  let target = new VexEventTarget();
 
-    let fn1 = jest.fn();
-    let fn2 = jest.fn();
+  let fn1 = jest.fn();
+  let fn2 = jest.fn();
 
-    target.remove("test1", fn1);
+  target.remove("test1", fn1);
 
-    target.emit("test1", undefined);
+  target.emit("test1", undefined);
 
+  target.on("test", fn1);
 
-    target.on("test", fn1);
+  let r1 = getRandomInt(10) + 10;
+  for (let i = 0; i < r1; i++) target.emit("test", undefined);
 
-    let r1 = getRandomInt(10) + 10;
-    for (let i = 0; i < r1; i++) target.emit("test", undefined);
+  target.on("test", fn2);
 
-    target.on("test", fn2);
+  let r2 = getRandomInt(10) + 10;
+  for (let i = 0; i < r2; i++) target.emit("test", undefined);
 
-    let r2 = getRandomInt(10) + 10;
-    for (let i = 0; i < r2; i++) target.emit("test", undefined);
+  expect(fn1).toBeCalledTimes(r1 + r2);
+  expect(fn2).toBeCalledTimes(r2);
 
-    expect(fn1).toBeCalledTimes(r1 + r2);
-    expect(fn2).toBeCalledTimes(r2);
+  target.remove("test", fn1);
 
-    target.remove("test", fn1);
+  target.emit("test", undefined);
 
-    target.emit("test", undefined);
+  expect(fn1).toBeCalledTimes(r1 + r2);
+  expect(fn2).toBeCalledTimes(r2 + 1);
 
-    expect(fn1).toBeCalledTimes(r1 + r2);
-    expect(fn2).toBeCalledTimes(r2 + 1);
+  target.clearListeners();
 
-    target.clearListeners();
+  target.emit("test", undefined);
 
-    target.emit("test", undefined);
-
-    expect(fn1).toBeCalledTimes(r1 + r2);
-    expect(fn2).toBeCalledTimes(r2 + 1);    
+  expect(fn1).toBeCalledTimes(r1 + r2);
+  expect(fn2).toBeCalledTimes(r2 + 1);
 });
